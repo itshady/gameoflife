864422611e6474f761c66b2a4162d4f80c7f2a87
Kareem Sultan <kareemsultan@gmail.com>
2022-07-29 15:23:28 -0400

    failing first gol test

--- src/test/game/GameOfLife.test.js
+++ src/test/game/GameOfLife.test.js
@@ -13,3 +13,16 @@ it('upon creation, initial state should match size and all dead', () => {
   const twoDsum = map.reduce((r, x) => r + x.reduce((s, y) => s + y, 0), 0)
   expect(twoDsum).toEqual(0)
 })
+
+it('gives a live cell with no surrounding live cells, expect dead cell', () => {
+  const expectedWidth = 5
+  const expectedHeight = 5
+  const game = new GameOfLife(expectedWidth, expectedHeight)
+  const map = game.mapData
+  map[3][3] = 1
+  expect(map[3][3]).toEqual(1)
+  game.NextGeneration()
+
+  const actualValue = game.map[3][3]
+  expect(actualValue).toEqual(0)
+})
