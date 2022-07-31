3581a5c2159fa9f7e60ace8cf43c34734f698939
Hady Ibrahim <hady.ibrahim@shopify.com>
2022-07-31 00:18:12 -0400

    working start, stop button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+// Bootstrap Components
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+
+// This Projects Components
 import Grid from './components/Grid'
 import GameOfLife from './game/GameOfLife'
 
@@ -14,20 +21,35 @@ function App() {
   // eslint-disable-next-line no-multi-assign, max-len
   mapPointer[3][3] = mapPointer[2][3] = mapPointer[3][2] = mapPointer[2][1] = mapPointer[1][3] = mapPointer[0][0] = 1
   const [map, setMap] = useState(mapPointer)
+  const [intervalId, setIntervalId] = useState(0)
 
-  useEffect(() => {
-    const interval = setInterval(() => {
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     gameLoop(setMap, game)
+  //   }, 1000)
+  //   return () => clearInterval(interval)
+  // }, [])
+
+  const handleClick = () => {
+    if (intervalId) {
+      clearInterval(intervalId)
+      setIntervalId(0)
+      return
+    }
+
+    const newIntervalId = setInterval(() => {
       gameLoop(setMap, game)
     }, 1000)
-    return () => clearInterval(interval)
-  }, [])
+    setIntervalId(newIntervalId)
+  }
 
   return (
-    <div className="App">
+    <Container className="app d-flex flex-column align-items-center">
       <Grid
         mapData={map}
       />
-    </div>
+      <Button onClick={handleClick}>{intervalId ? 'Stop' : 'Start'}</Button>
+    </Container>
   )
 }
 
