b18f9cecf5144af13e51b6637a0b9d2900112beb
Hady Ibrahim <hady.ibrahim@shopify.com>
2022-08-01 13:51:23 -0400

    add cell turning on AND off

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -9,7 +9,7 @@ function Cell({
     const a = e.target.parentElement.getAttribute('data-key')
     const b = e.target.getAttribute('data-key')
     const newMap = map
-    newMap[parseInt(a, 10)][parseInt(b, 10)] = 1
+    newMap[parseInt(a, 10)][parseInt(b, 10)] = state ? 0 : 1
     setMap(newMap)
     console.log(map)
   }
