2f2f51871e4c97c812a51079bc57970fb27f27ea
Hady Ibrahim <hady.ibrahim@shopify.com>
2022-08-03 14:39:59 -0400

    get front-end to update automatically, and interval time to change dynamically

--- src/App.js
+++ src/App.js
@@ -11,32 +11,34 @@ import { MdNextPlan, MdOutlineSpeed } from 'react-icons/md'
 import Grid from './components/Grid'
 import GameOfLife from './game/GameOfLife'
 
-function gameLoop(setMap, game, setGenerationCount) {
-  game.nextGeneration()
-  setMap(game.mapData)
-  setGenerationCount((generation) => generation + 1)
-}
+const MAXINTERVAL = 5000
 
 function App() {
   const [game] = useState(new GameOfLife(11, 11))
-  const mapPointer = game.mapData
-  useEffect(() => {
-    // eslint-disable-next-line no-multi-assign, max-len
-    mapPointer[3][3] = mapPointer[2][3] = mapPointer[3][2] = mapPointer[2][1] = mapPointer[1][3] = mapPointer[0][0] = 1
-  }, [])
 
-  const [map, setMap] = useState(mapPointer)
+  const [map, setMap] = useState(game.mapData)
   const [intervalId, setIntervalId] = useState(0)
   const [generationCount, setGenerationCount] = useState(0)
   // eslint-disable-next-line no-unused-vars
   const [intervalTime, setIntervalTime] = useState(1000)
 
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     gameLoop(setMap, game)
-  //   }, 1000)
-  //   return () => clearInterval(interval)
-  // }, [])
+  const gameLoop = () => {
+    game.nextGeneration()
+    setMap(game.mapData)
+    setGenerationCount((generation) => generation + 1)
+  }
+
+  const updateMap = (newMap) => {
+    setMap(newMap)
+    game.mapData = newMap
+  }
+
+  useEffect(() => {
+    const mapPointer = JSON.parse(JSON.stringify(game.mapData))
+    // eslint-disable-next-line no-multi-assign
+    mapPointer[3][3] = mapPointer[2][3] = mapPointer[3][2] = mapPointer[2][1] = mapPointer[1][3] = mapPointer[0][0] = 1
+    updateMap(mapPointer)
+  }, [])
 
   const handleLoop = () => {
     if (intervalId) {
@@ -56,14 +58,21 @@ function App() {
   }
 
   const handleInterval = (e) => {
-    setIntervalTime(e.target.value)
+    setIntervalTime(MAXINTERVAL - e.target.value)
+    if (intervalId) {
+      clearInterval(intervalId)
+      const newIntervalId = setInterval(() => {
+        gameLoop(setMap, game, setGenerationCount)
+      }, intervalTime)
+      setIntervalId(newIntervalId)
+    }
   }
 
   return (
     <div className="app d-flex flex-column align-items-center">
       <Grid
         mapData={map}
-        setMap={setMap}
+        setMap={updateMap}
       />
       <div className="app d-flex flex-row justify-content-between button-container">
         <Button onClick={handleLoop} key="button-start">{intervalId ? 'Stop' : 'Start'}</Button>
@@ -71,7 +80,7 @@ function App() {
         <div>{generationCount}</div>
         <Form.Label><MdNextPlan /></Form.Label>
         <Form.Label><MdOutlineSpeed /></Form.Label>
-        <Form.Range value={intervalTime} onChange={handleInterval} min={0} max={2000} />
+        <Form.Range value={MAXINTERVAL - intervalTime} onChange={handleInterval} min={0} max={MAXINTERVAL} />
       </div>
     </div>
   )
--- src/components/Cell.js
+++ src/components/Cell.js
@@ -7,14 +7,12 @@ function Cell({
   const handleClick = (e) => {
     const a = e.target.parentElement.getAttribute('data-key')
     const b = e.target.getAttribute('data-key')
-    const newMap = map
+    const newMap = JSON.parse(JSON.stringify(map))
     newMap[parseInt(a, 10)][parseInt(b, 10)] = state ? 0 : 1
     setMap(newMap)
-    console.log(map)
   }
 
   return (
-    // eslint-disable-next-line max-len
     // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions
     <td className={`cell ${state ? 'alive' : 'dead'}`} data-testid="cell" onClick={handleClick} data-key={cellKey} />
   )
