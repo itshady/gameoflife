da5e88c4b06183d407a22725589cf2eae5ab59b9
Hady Ibrahim <62480520+itshady@users.noreply.github.com>
2022-08-09 14:33:46 -0400

    Start/Stop button to run the game (#12)
    * Add Bootstrap to Project
    
    * working start, stop button
    
    * realized that without useState, the variable is redeclared every time anything changes, aka the component rerenders

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+// Bootstrap Components
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+
+// This Projects Components
 import Grid from './components/Grid'
 import GameOfLife from './game/GameOfLife'
 
@@ -9,25 +16,43 @@ function gameLoop(setMap, game) {
 }
 
 function App() {
-  const game = new GameOfLife(11, 11)
+  const [game] = useState(new GameOfLife(11, 11))
   const mapPointer = game.mapData
-  // eslint-disable-next-line no-multi-assign, max-len
-  mapPointer[3][3] = mapPointer[2][3] = mapPointer[3][2] = mapPointer[2][1] = mapPointer[1][3] = mapPointer[0][0] = 1
-  const [map, setMap] = useState(mapPointer)
-
   useEffect(() => {
-    const interval = setInterval(() => {
-      gameLoop(setMap, game)
-    }, 1000)
-    return () => clearInterval(interval)
+    // eslint-disable-next-line no-multi-assign, max-len
+    mapPointer[3][3] = mapPointer[2][3] = mapPointer[3][2] = mapPointer[2][1] = mapPointer[1][3] = mapPointer[0][0] = 1
   }, [])
 
+  const [map, setMap] = useState(mapPointer)
+  const [intervalId, setIntervalId] = useState(0)
+
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     gameLoop(setMap, game)
+  //   }, 1000)
+  //   return () => clearInterval(interval)
+  // }, [])
+
+  const handleClick = () => {
+    if (intervalId) {
+      clearInterval(intervalId)
+      setIntervalId(0)
+      return
+    }
+
+    const newIntervalId = setInterval(() => {
+      gameLoop(setMap, game)
+    }, 1000)
+    setIntervalId(newIntervalId)
+  }
+
   return (
-    <div className="App">
+    <Container className="app d-flex flex-column align-items-center">
       <Grid
         mapData={map}
       />
-    </div>
+      <Button onClick={handleClick}>{intervalId ? 'Stop' : 'Start'}</Button>
+    </Container>
   )
 }
 
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,4 +1,6 @@
 import React, { } from 'react'
+import '../assets/Grid.css'
+
 import Row from './Row'
 
 const validate = (mapData) => {
